5f649d41143a9677befe527512e47606467ef97f
genghiskhang <khang11475@gmail.com>
2022-02-18 19:40:03 -0500

    Updated team script to use default-avatar.jpg if image does not exist

--- js/team.js
+++ js/team.js
@@ -8,7 +8,7 @@ function createAvatars() {
         for (var i = 0; i < memberList.length; i++) {
             $('.members').append('<div class="member-profile">' +
                                     // '<img class="avatar" src="../assets/member-avatars/default-avatar.jpg">' +
-                                    `<img class="avatar" src="../assets/member-avatars/${memberList[i].split('-')[0].split(' ')[0].toLowerCase() + memberList[i].split('-')[0].split(' ')[1][0].toLowerCase()}.jpg">` +
+                                    `<img class="avatar" src="../assets/member-avatars/${memberList[i].split('-')[0].split(' ')[0].toLowerCase() + memberList[i].split('-')[0].split(' ')[1][0].toLowerCase()}.jpg" onerror="this.src='../assets/member-avatars/default-avatar.jpg'">` +
                                     `<h1 class="member-info">${memberList[i].split('-')[0].trim()}</h1>` +
                                     `<h2 class="member-position">${memberList[i].split('-')[1].trim()}</h2>` +
                                  '</div>');
