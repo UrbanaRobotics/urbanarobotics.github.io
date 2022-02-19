aed059ca5e146bc5601266e7289647a9178b13d3
genghiskhang <khang11475@gmail.com>
2022-02-19 11:56:20 -0500

    Added socials to header and updated footer

--- js/header.js
+++ js/header.js
@@ -6,17 +6,24 @@ window.onscroll = scrollFunction;
 function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     document.getElementById('team-logo').style.maxHeight = '4vw';
-    document.getElementById('team-logo').style.margin = '10px 0px 10px 0px';
+    document.getElementById('team-logo').style.margin = '1vw 0vw 1vw 1vw';
     document.getElementById('team-name').style.fontSize = '2vw';
     document.getElementById('team-name').style.marginTop = '0px';
     document.getElementById('team-number').style.fontSize = '0vw';
+    document.getElementById('social').style.display = 'none';
+    // document.getElementById('social').style.flexDirection = 'row';
+    // document.getElementById('social').style.alignItems = 'center';
+    
   }
   else {
     document.getElementById('team-logo').style.maxHeight = '8vw';
-    document.getElementById('team-logo').style.margin = '20px 0px 20px 0px';
+    document.getElementById('team-logo').style.margin = '2vw 0vw 2vw 0vw';
     document.getElementById('team-name').style.fontSize = '3vw';
     document.getElementById('team-name').style.marginTop = '20px';
     document.getElementById('team-number').style.fontSize = '2vw';
+    document.getElementById('social').style.display = 'flex';
+    // document.getElementById('social').style.flexDirection = 'column';
+    // document.getElementById('social').style.margin = '2.5vw 0vw 2.5vw 0vw';
   }
 }
 
