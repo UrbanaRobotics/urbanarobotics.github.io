0e024e1001e281c56fb84b5bb46d4288506634da
genghiskhang <khang11475@gmail.com>
2022-02-19 14:43:52 -0500

    Fixed text and image scaling on home page

--- js/header.js
+++ js/header.js
@@ -1,7 +1,4 @@
-const MIN_HEADER_WIDTH = 1280;
-
 window.onscroll = scrollFunction;
-// body.onresize = adjustHeader;
 
 function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
@@ -11,12 +8,8 @@ function scrollFunction() {
     document.getElementById('team-name').style.marginTop = '0px';
     document.getElementById('team-number').style.fontSize = '0vw';
     for (var i = 0; i < document.getElementsByClassName('social-logo').length; i++) {
-      console.log( document.getElementsByClassName('social-logo')[i]);
-      document.getElementsByClassName('social-logo')[i].style.width = '0vw';
+      document.getElementsByClassName('social-logo')[i].style.width = '0vw'
     }
-    // document.getElementById('social').style.flexDirection = 'row';
-    // document.getElementById('social').style.alignItems = 'center';
-    
   }
   else {
     document.getElementById('team-logo').style.maxHeight = '8vw';
@@ -25,20 +18,7 @@ function scrollFunction() {
     document.getElementById('team-name').style.marginTop = '20px';
     document.getElementById('team-number').style.fontSize = '2vw';
     for (var i = 0; i < document.getElementsByClassName('social-logo').length; i++) {
-      console.log( document.getElementsByClassName('social-logo')[i]);
       document.getElementsByClassName('social-logo')[i].style.width = '1.5vw';
     }
-    // document.getElementById('social').style.display = 'flex';
-    // document.getElementById('social').style.flexDirection = 'column';
-    // document.getElementById('social').style.margin = '2.5vw 0vw 2.5vw 0vw';
   }
 }
-
-// function adjustHeader() {
-//   if (body.clientWidth <= MIN_HEADER_WIDTH) {
-
-//   }
-//   if (body.clientWidth > MIN_HEADER_WIDTH) {
-    
-//   }
-// }
